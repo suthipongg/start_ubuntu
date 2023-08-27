afbae7bc15b159d27497bb7fa767d42b7aaef6a5
suthipongg <suthipong.mew@gmail.com>
2023-08-27 20:05:23 +0700

    edit name extention file

--- oh_my_zsh.sh
+++ oh_my_zsh.sh
@@ -11,9 +11,9 @@ chsh -s $(which zsh)
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 mv .zshrc .zshrc_backup
-cp $curr_dir/$dir_setup/config/config_oh_my_zsh $HOME/.zshrc
+cp $curr_dir/$dir_setup/config/config_oh_my_zsh.sh $HOME/.zshrc
 
 echo "-------------------------setup zsh---------------------------"
-echo "$(< $curr_dir/$dir_setup/config/config_zsh)" >> $HOME/.zshrc
+echo "$(< $curr_dir/$dir_setup/config/config_zsh.sh)" >> $HOME/.zshrc
 
 source .zshrc 
