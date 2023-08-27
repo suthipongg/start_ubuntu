d59511a5a720e32fee0b51c9e66c6b5efcc4567e
suthipongg <suthipong.mew@gmail.com>
2023-08-27 16:33:25 +0700

    add alias to zsh

--- oh_my_zsh.sh
+++ oh_my_zsh.sh
@@ -10,10 +10,18 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 mv .zshrc .zshrc_backup
 touch .zshrc
-echo 'export ZSH="$HOME/.oh-my-zsh"' >> .zshrc
-echo 'ZSH_THEME="agnoster"' >> .zshrc
-echo 'source $ZSH/oh-my-zsh.sh' >> .zshrc
-echo 'source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> .zshrc
-echo 'source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> .zshrc
-echo 'plugins=(git zsh-syntax-highlighting zsh-autosuggestions)' >> .zshrc
+
+echo "
+export ZSH='$HOME/.oh-my-zsh'
+ZSH_THEME='agnoster'
+source $ZSH/oh-my-zsh.sh
+source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+
+alias ez='vim \$HOME/.zshrc'
+alias sz='source \$HOME/.zshrc'
+alias gst='git status'
+" >> ~/.zshrc
+
 source .zshrc 
