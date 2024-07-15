e4ab816358934259f2a5679b4da9f13e89f160ce
suthipongg <119855840+suthipongg@users.noreply.github.com>
2024-07-15 17:44:18 +0700

    Update oh_my_zsh.sh

--- oh_my_zsh.sh
+++ oh_my_zsh.sh
@@ -9,8 +9,10 @@ echo "-------------------------install extension oh my zsh----------------------
 apt install powerline fonts-powerline -y
 echo "-------------------------setup oh my zsh---------------------------"
 chsh -s $(which zsh)
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
 mv .zshrc .zshrc_backup
 cp $curr_dir/$dir_setup/config/config_oh_my_zsh.sh $HOME/.zshrc
 
