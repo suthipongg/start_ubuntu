b58bb73f9252dd7efed2c189965d76576601bf9c
suthipongg <119855840+suthipongg@users.noreply.github.com>
2024-07-15 17:46:46 +0700

    Update config_oh_my_zsh.sh

--- config/config_oh_my_zsh.sh
+++ config/config_oh_my_zsh.sh
@@ -1,6 +1,25 @@
 export ZSH=$HOME/.oh-my-zsh
-ZSH_THEME='agnoster'
+
+ZSH_THEME='powerlevel10k/powerlevel10k'
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(history)
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+export LS_COLORS="rs=0:no=00:mi=00:mh=00:ln=01;36:or=01;31:di=01;34:ow=04;01;34:st=34:tw=04;34:pi=01;33:so=01;33:do=01;33:bd=01;33:cd=01;33:su=01;35:sg=01;35:ca=01;35:ex=01;32:"
+
+ZSH_CUSTOM="$ZSH/custom"
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor root line)
+ZSH_HIGHLIGHT_PATTERNS=('rm -rf *' 'fg=white,bold,bg=red')
+
+plugins=(
+    docker
+    git
+    github
+    gitignore
+    history-substring-search
+    vscode
+    sudo
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    ohmyzsh-full-autoupdate
+)
+
 source $ZSH/oh-my-zsh.sh
-source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
