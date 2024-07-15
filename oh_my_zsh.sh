1bc5d1d59c8babd52291dc02d7053ad51427b92a
suthipongg <119855840+suthipongg@users.noreply.github.com>
2024-07-15 17:49:20 +0700

    Update oh_my_zsh.sh

--- oh_my_zsh.sh
+++ oh_my_zsh.sh
@@ -20,3 +20,6 @@ echo "-------------------------setup zsh---------------------------"
 echo "$(< $curr_dir/$dir_setup/config/config_zsh.sh)" >> $HOME/.zshrc
 
 source $HOME/.zshrc 
+
+# uncomment command below if the config not showed
+# p10k configure
